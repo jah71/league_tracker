d78f7609816e6b41fac93018fff07c524397b8df
jah71 <jah71aber@gmail.com>
2022-09-04 13:09:36 +0100

    2-create-mvp: initial commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const [matches, setMatches] = useState([]);
+
+  function search(e) {
+    console.log('searching...');
+    e.preventDefault();
+
+    // TODO: get from riot api + move this to a services file
+    fetch('http://shibe.online/api/shibes?count=3')
+      .then((response) => response.json())
+      .then((e) => {
+        setMatches(e);
+      }).catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="main-content">
-          <div className="search-bar">
-            <span className="inp-summoner-name-search-bar">
-              Add a search bar here
-            </span>
-            <button className="btn-summoner-name-search">Search</button>
-          </div>
-          <div>
-            TODO: Table goes here
-          </div>
+    <div className="main-content">
+      <form name="get-stats" onSubmit={search}>
+        <div className="search-bar">
+          <label>Search </label>
+          <input name="search" placeholder="Summoner name" onChange={e => setSearchValue(e.target.value)} data-testid="inp-summoner-serach" />
+          <button type="submit" className="btn-summoner-name-search" data-testid="btn-summoner-serach">Search</button>
         </div>
-      </header>
-    </div>
+      </form>
+      <h2 className="last-games">
+        Last 10 games
+      </h2>
+      <div className="table-matches">
+        {matches.length > 0 ? (<table data-testid="table-match-history" className="table-matches">
+          <thead className="header-matches">
+            <tr>
+              <th colSpan="1">Result</th>
+              <th colSpan="1">Champion</th>
+              <th colSpan="1">K/D/A</th>
+            </tr>
+          </thead>
+          <tbody>
+            {matches.map((match, index) => (
+              <tr data-testid={'table-row-' + index} className="table-row" key={index}>
+                <td className="table-col">{match}</td>
+                <td className="table-col">Zac</td>
+                <td className="table-col">4/2/0</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>) : (
+          <div data-testid="no-matches-found" className="no-matches">
+            No matches, please search for a summoner above
+          </div>
+        )}
+      </div>
+    </div >
   );
 }
 
--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import * as React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  // TODO: mock api call here when we extract it to a service
+
+  const defaultProps = {
+    // TODO: add default props
+  };
+
+  function setup(props) {
+    return render(<App />, {
+      ...defaultProps,
+      ...props,
+    })
+  }
+
+  describe('@renders', () => {
+    test('search bar', () => {
+      const wrapper = setup();
+
+      const searchBar = wrapper.getByTestId('inp-summoner-serach');
+
+      expect(searchBar).toBeInTheDocument();
+    });
+
+    test('search button', () => {
+      const wrapper = setup();
+
+      const searchButton = wrapper.getByTestId('btn-summoner-serach');
+      // TODO: once the api is extracted/mocked we need to click this button and expect it to have been called
+
+      expect(searchButton).toBeInTheDocument();
+    });
+
+    test('no matches found', () => {
+      const wrapper = setup();
+
+      const noMatchesFound = wrapper.getByTestId('no-matches-found');
+
+      expect(noMatchesFound).toBeInTheDocument();
+    });
+
+    test('table', () => {
+      const wrapper = setup();
+      // TODO: stub the api so the table can be populated
+
+      // TODO: locate table
+      // TODO: expect table to have some elements
+    });
+  });
 });
